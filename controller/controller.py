02ab8ca2647378acc7d24b9b8202542234b22ee2
dcuentas <dcuentas@gurpoalto.com>
2021-10-30 21:27:11 -0500

    custom output filename & using permutations instead of combinations

--- controller/controller.py
+++ controller/controller.py
@@ -8,11 +8,11 @@ class Controller:
     """Object controller."""
 
     @classmethod    
-    def worker(cls):
-        logging.info(f'[INIT] python_ntf_generator worker')
+    def worker(cls, output_name):
+        logging.info(f'[INIT] python_digital_art_generator worker')
         
         try:
-            f = open('config.json',)
+            f = open('config.json')
         except Exception as e:
             logging.error(e)
             return
@@ -26,19 +26,22 @@ class Controller:
         resources_path = 'resources/'
         filenames_list = os.listdir(resources_path)
         filenames_list = list(map(lambda x: x.split(".")[0], filenames_list))
-
         collections = []
-        combinations = itertools.combinations(filenames_list, 4)
-        for x in combinations:
+        permutations = itertools.permutations(filenames_list, 4)
+        for x in permutations:
             if cls.check_hierarchy(x, layers_json_list):
                 collections.append(x)
-        cls.build_images(collections)
+        cls.build_images(collections, output_name)
 
     @classmethod
     def check_hierarchy(cls, possible_collection, layers_json_list):
         last_level = cls.get_image_level(possible_collection[0], layers_json_list)
+        print('Current collection: ', possible_collection)
         for x in range(1, len(possible_collection)):
             current_level = cls.get_image_level(possible_collection[x], layers_json_list)
+            print(last_level)
+            print(current_level)
+            print('----')
             if last_level >= current_level:
                 return False
             last_level = current_level
@@ -51,7 +54,7 @@ class Controller:
                return int(layer.split("_")[1])
     
     @classmethod
-    def build_images(cls, collections):
+    def build_images(cls, collections, output_name):
         logging.info(f'[STEP] building imagenes')
         resources_extension = 'png'
         for index, collection in enumerate(collections):
@@ -59,5 +62,5 @@ class Controller:
             for img in collection:
                 image_to_merge = Image.open(f'resources/{img}.{resources_extension}')
                 nft_collection_image  = Image.alpha_composite(nft_collection_image,image_to_merge)
-            nft_collection_image.save(f'collections/ntf_no_{index}.png',"PNG")
+            nft_collection_image.save(f'collections/{output_name}_no_{index}.png',"PNG")
         logging.info(f'[STEP] building imagenes finished')
